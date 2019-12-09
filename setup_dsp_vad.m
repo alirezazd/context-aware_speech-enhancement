4f9d3f7b71a48f89c05354fcddea2209c9f96d14
alirezazd <azadi_ali78@yahoo.ca>
2019-12-09 19:25:50 +0330

    Primary blocks of VAD added and tested.

--- setup_dsp_vad.m
+++ setup_dsp_vad.m
@@ -8,8 +8,9 @@
 dspb_vad.ClockRate = 50;
 dspb_vad.ClockMargin = 0.0;
 dspb_vad.SampleTime  = 1;
-temp = [1:10];
 
+SF = 16000; %sampling frequency
+FS = 512;   %frame size
 
 %% FFT data
 dspb_vad.IFFT = false;
@@ -33,13 +34,11 @@ dspb_vad.type_strings = ...
 dspb_vad.SimulationTime = 512;
 
 %% Generate some input samples
-dspb_vad.sampledata = ...
-    generate_fft_sample_data(...
-        dspb_vad.SimulationTime, ...
-        dspb_vad.blockSize, ...
-        [2.3, 0.1; 60, 0.1; 950.2, 0.1], ...
-        0.2);
-
+dspb_vad.sampledata = 256*sin(2*pi*440*(0:1/SF:0.025-1/SF));  %25ms sin wave
+%dspb_vad.sampledata = (dspb_vad.sampledata'.*hamming(400)).' %apply hamming window
+%dspb_vad.sampledata = dspb_vad.sampledata + 50*randn(1, length(dspb_vad.sampledata)); %add noise
+dspb_vad.sampledata(1,FS) = 0; %zero pad to 512 samples
+%use example script to generate samples -> generate_fft_sample_data(dspb_vad.SimulationTime, dspb_vad.blockSize, [2.3, 0.1; 60, 0.1; 950.2, 0.1],0.2);
 dspb_vad.bus_addr_in=[];
 dspb_vad.bus_data_in = [];
 dspb_vad.bus_wen = [];
@@ -52,7 +51,7 @@ while(dspb_vad.point < 512)
     count = count + 1;
     dspb_vad.bus_addr_in(1,count) = 1;
     dspb_vad.point = dspb_vad.point + 1;
-    dspb_vad.bus_data_in(1,count) = abs(dspb_vad.sampledata(1,dspb_vad.point));
+    dspb_vad.bus_data_in(1,count) = dspb_vad.sampledata(1,dspb_vad.point);
     dspb_vad.bus_wen(1,count)=1;
     count = count + 1;
     dspb_vad.bus_addr_in(1,count) = 0;
