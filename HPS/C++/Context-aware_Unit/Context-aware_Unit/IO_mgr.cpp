5210537b7b690f642b4caccbc085ffb505b16d00
alirezazd <azadi_ali78@yahoo.ca>
2021-01-12 19:53:34 +0330

    *Fully functional prorype.

--- HPS/C++/Context-aware_Unit/Context-aware_Unit/IO_mgr.cpp
+++ HPS/C++/Context-aware_Unit/Context-aware_Unit/IO_mgr.cpp
@@ -162,50 +162,6 @@ float IO_mgr::Read_SNR_dB2() {
 		return 9;
 	case 3:
 		return 8;
-	case 4:
-		return 10;
-	case 5:
-		return 7;
-	case 6:
-		return 8;
-	case 7:
-		return 7;
-	case 8:
-		return 4;
-	case 9:
-		return 5;
-	case 10:
-		return 4;
-	case 11:
-		return 3;
-	case 12:
-		return 1;
-	case 13:
-		return 1;
-	case 14:
-		return 0;
-	case 15:
-		return 1;
-	case 16:
-		return 1;
-	case 17:
-		return 1;
-	case 18:
-		return 0;
-	case 19:
-		return 1;
-	case 20:
-		return 3;
-	case 21:
-		return 3;
-	case 22:
-		return 4;
-	case 23:
-		return 4;
-	case 24:
-		return 6;
-	case 25:
-		return 6;
 	}
 	return 0;
 }
@@ -221,16 +177,6 @@ int IO_mgr::Read_location() {
 		return WHITE_NOISE;
 	case 3:
 		return VOLVO_NOISE;
-	case 4:
-		return BUCCANEER2_NOISE;
-	case 5:
-		return VOLVO_NOISE;
-	case 6:
-		return VOLVO_NOISE;
-	case 7:
-		return WHITE_NOISE;
-	case 8:
-		return WHITE_NOISE;
 	}
 	return UNKNOWN_NOISE;
 }
